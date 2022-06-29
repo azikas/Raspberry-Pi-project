f065005b974541b57a50ba730c7dea8b1b3d194f
azikas <108398360+azikas@users.noreply.github.com>
2022-06-29 23:26:16 +0300

    Add files via upload

---
+++ RaspberryPi1.py
@@ -0,0 +1,78 @@
+# Write your code here :-)
+import socket
+from Crypto.Cipher import AES
+
+#public and private keys
+P=23
+G=9
+a=4
+
+def generate_key(base, power, modulo):
+    key = int(pow(base, power, modulo))
+    return key
+
+def module1():
+
+    #compute key
+    x = generate_key(G, a, P)
+
+    #send private key
+    clientconnected.send(str(x).encode())
+    print("Sending key...\n")
+
+    #receive private key
+    y = clientconnected.recv(16)
+    print("Receiving key...\n")
+
+    #symmetric key->16 bytes
+    ka = generate_key(int(y), a, P)
+    ka = str(ka).encode()
+    ka = ka.zfill(16)
+
+    return ka
+
+def module2(key):
+
+    #read image
+    plain_img = open("/home/pi/mu_code/images/initial_img.jpg",'rb')
+    plain_data = plain_img.read()
+    plain_data = plain_data[: - (len(plain_data)%16) ]
+    plain_img.close()
+
+    #encrypt data
+    cipher = AES.new( key , AES.MODE_ECB)
+    enc_data = cipher.encrypt(plain_data)
+    print("Encrypting data...\n")
+
+    #create encrypted image
+    enc_img = open("enc_img.jpg","wb")
+    enc_img.write(enc_data)
+    enc_img.close()
+
+    #send encrypted image
+    print("Sending encrypted image...\n")
+    clientconnected.sendall(enc_data)
+    print("Sent %s bytes." %len(enc_data))
+
+    clientconnected.close()
+
+
+#Main program
+
+#open socket
+serversocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+serversocket.bind(("169.254.193.224", 9090))
+serversocket.listen()
+
+#connect with client
+(clientconnected, clientaddress) = serversocket.accept()
+print("Accepted a connection request. \n")
+
+symmetric_key = module1()
+
+module2(symmetric_key)
+
+serversocket.close()
+
+
+
---
+++ RaspberryPi2.py
@@ -0,0 +1,76 @@
+# Write your code here :-)
+import socket
+from Crypto.Cipher import AES
+
+#public and private keys
+P=23
+G=9
+b=3
+
+def generate_key(base, power, modulo):
+    key = int(pow(base, power, modulo))
+    return key;
+
+def module1():
+
+    #compute key
+    y = generate_key(G, b, P);
+
+    #receive private key
+    x = clientsocket.recv(16)
+    print("Receiving key...\n")
+
+    #send private key
+    clientsocket.send(str(y).encode())
+    print("Sending key...\n")
+
+    #symmetric key
+    kb = generate_key(int(x), b, P)
+    kb = str(kb).encode()
+    kb = kb.zfill(16)
+
+    return kb
+
+
+def module2(key):
+
+    #receive encrypted data
+    rcv_img = open("received_img.jpg", "wb")
+    print("Receiving encrypted data...\n")
+    r = clientsocket.recv(1024)
+
+    #wait all data
+    i = len(r)
+    while(r):
+        rcv_img.write(r)
+        r = clientsocket.recv(1024)
+        i = i + len(r)
+    print("Received %s bytes. \n" %i)
+    rcv_img.close()
+
+    #read encrypted data
+    rcv_img = open("received_img.jpg", "rb")
+    enc_data = rcv_img.read()
+    rcv_img.close()
+
+    #decrypt data
+    decipher = AES.new( key, AES.MODE_ECB)
+    dec_data = decipher.decrypt(enc_data)
+    print("Decrypting data..\n")
+
+    #store decrypted image
+    dec_img = open("decrypted_img.jpg", "wb")
+    dec_img.write(dec_data)
+    dec_img.close()
+
+
+#Main program
+
+#connect with server
+clientsocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+clientsocket.connect(("169.254.193.224", 9090))
+print("Requesting a connection...\n")
+
+symmetric_key = module1()
+
+module2(symmetric_key)
